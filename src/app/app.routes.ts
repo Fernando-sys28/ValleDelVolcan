7a070ca67bb74087f0d922b5ef95b450cea876a9
Fernando Rivera Castillo <116294941+Fernando-sys28@users.noreply.github.com>
2024-04-19 10:45:15 -0600

    routes edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,8 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
   { path: 'proceso-purificacion', component: ProcesoPurificacionComponent },
-  { path: 'delivery-form', component: DeliveryFormComponent },
-  {path: '', component: HomeComponent},
-  // Other existing routes...
-=======
-  {path: 'delivery', component: DeliveryFormComponent },
   {path: 'contactanos', component: ContactPageComponent },
   {path: '', component: MainPageComponent},
->>>>>>> bf4495f (parte 2)
+
 ];
