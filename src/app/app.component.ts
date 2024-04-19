4e7838d4fc7c72d25c73f12fb1fa8d4da7097f20
hectorBoi <hector.emiliano.zg@gmail.com>
2024-04-18 22:01:33 -0600

    added home component and delivery form linked button to delivery form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app-pf-valle-volcan';
+
+  constructor(private router: Router) { }
+
+  navigateToDeliveryForm() {
+    this.router.navigate(['/delivery-form']);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DeliveryFormComponent } from './delivery-form/delivery-form.component';
+import { HomeComponent } from './home/home.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'delivery-form', component: DeliveryFormComponent },
+  {path: '', component: HomeComponent},
+  // Other existing routes...
+];
