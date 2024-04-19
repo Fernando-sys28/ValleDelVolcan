dacb93da4a43564444b802727bfaab1a1900af31
Fernando Rivera Castillo <116294941+Fernando-sys28@users.noreply.github.com>
2024-04-19 10:56:06 -0600

    cambios hector

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
+  {path: 'delivery', component: DeliveryFormComponent },
   { path: 'proceso-purificacion', component: ProcesoPurificacionComponent },
   {path: 'contactanos', component: ContactPageComponent },
   {path: '', component: MainPageComponent},
-
 ];
