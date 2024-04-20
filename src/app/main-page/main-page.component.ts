8ddb159b0a92c6deb294ee8ea4d7e34fca50ea7b
Fernando Rivera Castillo <116294941+Fernando-sys28@users.noreply.github.com>
2024-04-20 00:41:06 -0600

    mejoras

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { RouterModule, ActivatedRoute} from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-page',
@@ -8,6 +10,20 @@ import { RouterModule } from '@angular/router';
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private viewportScroller: ViewportScroller
+  ) {}
 
+  ngOnInit(): void {
+    // Escucha los cambios de fragmentos
+    this.route.fragment.pipe(
+      filter(fragment => !!fragment)
+    ).subscribe(fragment => {
+      // Espera al ciclo de vida del navegador para que complete la navegación
+      // Puede requerir ajustes dependiendo de tu aplicación
+      setTimeout(() => this.viewportScroller.scrollToAnchor(fragment!), 100);
+    });
+  }
 }
